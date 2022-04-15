b06f21621825d8a43f7100bb2c1c6047aac0afc2
Ilya <ilya-lyapin@mail.ru>
2022-04-15 22:17:44 +0300

    Init project

---
+++ AdminPanel/client/src/reducers/userReducer.js
@@ -0,0 +1,14 @@
+
+
+const  defaultState = {
+    currentuser: {},
+    isAuth : false
+}
+
+export default  function userReducer(state = defaultState, action) {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
