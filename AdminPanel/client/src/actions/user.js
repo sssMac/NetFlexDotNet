f647ce139d381dec915177ec326b1e382f438f73
ali <aliaqel2021@gmail.com>
2022-04-18 01:56:20 +0300

    change "server error" to e.massage

--- AdminPanel/client/src/actions/user.js
+++ AdminPanel/client/src/actions/user.js
@@ -2,26 +2,14 @@ import axios from "axios";
 
 export const registration = async (email, password, confirmPassword) => {
   try {
-    const response = await axios({
-      method: "post",
-      url: "http://localhost:5000/auth/registration",
-      headers: {},
-      data: {
+    const response = await axios.post(
+      "http://localhost:5000/auth/registration",
+      {
         email,
         password,
         confirmPassword,
-      },
-    });
-
-    // const response = await axios.post(
-    //   "http://localhost:5000/auth/registration",
-    //   {
-    //     email,
-    //     password,
-    //     confirmPassword,
-    //   }
-    // );
-    console.log(response);
+      }
+    );
     alert(response.data.message);
   } catch (e) {
     alert(e);
