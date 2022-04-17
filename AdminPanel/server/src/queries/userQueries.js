a1d1732bd1c3ad3187a0ebb828a617398ef175ff
ali <aliaqel2021@gmail.com>
2022-04-17 18:39:21 +0300

    add LoginProvider

--- AdminPanel/server/src/queries/userQueries.js
+++ AdminPanel/server/src/queries/userQueries.js
@@ -3,11 +3,12 @@ const getUserIdByToken = 'select "UserId" from "AspNetUserTokens" where "Value" 
 const getRoleName = 'select "Name" from "AspNetRoles" r join "AspNetUserRoles" ur on r."Id" =ur."RoleId" where ur."RoleId" = $1';
 const findUser = 'select "Id","Avatar","UserName","NormalizedUserName","Email","NormalizedEmail","EmailConfirmed","PasswordHash" from "AspNetUsers" where "Email" = $1';
 const addUser = 'insert into "AspNetUsers" ("Id","Email","PasswordHash","EmailConfirmed") values ($1,$2,$3,true);';
-const saveToken = 'insert into "AspNetUserTokens" ("UserId", "LoginProvider", "Name", "Value") values ($1,\'login\',\'accessToken\',$2)';
+const saveToken = 'insert into "AspNetUserTokens" ("UserId", "LoginProvider", "Name", "Value") values ($1,\'user\',\'accessToken\',$2)';
+
 module.exports = {
-  getUserIdByToken,
-  getRoleName,
-  findUser,
-  addUser,
-  saveToken
+    getUserIdByToken,
+    getRoleName,
+    findUser,
+    addUser,
+    saveToken
 };
