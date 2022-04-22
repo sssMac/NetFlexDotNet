0f0b9695bc22499cdd3b4b6b4fa22f379a8dcfe0
Raw1aN <shchukin2002@yandex.ru>
2022-04-22 16:12:03 +0300

    feature : add user system

---
+++ NetFlexAPI/NetFlexAPI/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetFlexAPI.Models;
+
+namespace NetFlexAPI.Data;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+ 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("User ID=postgres; Server=localhost; port=5432; Database=NetFlexDb; Password=ALEXRED123321!@; Pooling=true;");
+    }
+}
