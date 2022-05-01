58cf420efc4a890ad3d298bb5a357670cc5b85d2
ali <aliaqel2021@gmail.com>
2022-05-01 19:17:15 +0300

    added randomUUID to role

--- AdminPanel/server/src/controllers/rolesController.js
+++ AdminPanel/server/src/controllers/rolesController.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const createError = require("../errors/createError");
 const db = require("../database/db");
 const queries = require("../queries/rolesQueries");
+const {randomUUID} = require("crypto");
 
 class RolesController {
 
@@ -19,12 +20,11 @@ class RolesController {
 
     async createRole(req,res,next){
         try {
-            const {roleId, roleName} = req.body
-            const results = await db.query(queries.addRoles, [roleId, roleName])
+            const {roleName} = req.body
+            const results = await db.query(queries.addRoles, [randomUUID(), roleName])
             if (results.error)
                 return next(new createError(401, results.error))
             return res.status(200).json({
-               roleId : roleId,
                 roleName : roleName
             })
         }catch (e) {
