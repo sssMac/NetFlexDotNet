68434ce78137fe07ce4b581d74a4fa8750b3a7e0
Raw1aN <shchukin2002@yandex.ru>
2022-05-08 19:18:10 +0300

    Add Tests

---
+++ NetFlexAPI/TestProject/CreateUserRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace TestProject;
+
+public class CreateUserRequest
+{
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+
+    public CreateUserRequest(string email, string passwordHash)
+    {
+        Email = email;
+        PasswordHash = passwordHash;
+    }
+}
