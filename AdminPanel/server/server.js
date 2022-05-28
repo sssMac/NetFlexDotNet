4d32b33d11870a4e6189263cc5bb8274c0aa4be1
Olesya-Gerasimova <ogerasimova966@gmail.com>
2022-05-29 00:05:38 +0300

    added episode files

--- AdminPanel/server/server.js
+++ AdminPanel/server/server.js
@@ -7,6 +7,7 @@ const userRouter = require('./src/routes/userRouter')
 const corsMiddleware = require('./middleware/corsMiddleware')
 const genreRouter = require('./src/routes/genreRouter');
 const seriesRouter = require('./src/routes/seriesRouter');
+const episodesRouter = require('./src/routes/episodesRouter');
 const reviewRouter = require('./src/routes/reviewRouter');
 
 const app = express();
@@ -25,6 +26,7 @@ app.use("/user", userRouter);
 app.use("/genre", genreRouter);
 app.use("/review", reviewRouter);
 app.use("/series", seriesRouter);
+app.use("/episodes", episodesRouter)
 
 // home handler
 app.get("/", (res, rep) => {
