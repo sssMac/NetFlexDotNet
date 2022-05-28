06ecbfb888d489a33cf35ba47f00c624c0d032bd
Olesya-Gerasimova <ogerasimova966@gmail.com>
2022-05-28 21:50:13 +0300

    Add update of serials parameters

--- AdminPanel/server/src/queries/seriesQueries.js
+++ AdminPanel/server/src/queries/seriesQueries.js
@@ -1,8 +1,8 @@
 // this file for admin sql queries ...
 const addSerial = 'insert into "Serials" ("Id", "Poster", "Title", "NumEpisodes", "AgeRating", "UserRating", "Description") values ($1,$2,$3,$4,$5,$6,$7)';
 const deleteSerial = 'delete from "Serials" where "Id" = $1'
-const renameSerial = 'update "Serials" set "SerialName" = $1 where "Id" = $2';
-const findSerial = 'select * from "Serials" where "SerialName" = $1';
+const updateSerial = 'update "Serials" set "Poster" = $2, "Title" = $3, "NumEpisodes" = $4, "AgeRating" = $5, "UserRating" = $6, "Description" = $7 where "Id" = $1';
+const findSerial = 'select * from "Serials" where "Title" = $1';
 const findSerialById = 'select * from "Serials" where "Id" = $1';
 const getAllSerials = 'select * from "Serials"';
 
@@ -10,7 +10,7 @@ const getAllSerials = 'select * from "Serials"';
 module.exports = {
   addSerial,
   deleteSerial,
-  renameSerial,
+  updateSerial,
   findSerial,
   findSerialById,
   getAllSerials
