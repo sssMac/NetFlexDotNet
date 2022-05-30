ea82eaf16a96f4d7b5df7ea401e35870655a2136
Olesya-Gerasimova <ogerasimova966@gmail.com>
2022-05-30 11:25:37 +0300

    added changes

--- AdminPanel/server/src/queries/episodeQueries.js
+++ AdminPanel/server/src/queries/episodeQueries.js
@@ -4,7 +4,7 @@ const deleteEpisode = 'delete from "Episodes" where "Id" = $1'
 const updateEpisode = 'update "Episodes" set Title" = $2, "SerialId" = $3, "Duration" = $4, "Number" = $5, "VideoLink" $6, "PreviewVideo" = $7 where "Id" = $1';
 const findEpisode = 'select * from "Episodes" where "Title" = $1';
 const findEpisodeById = 'select * from "Episodes" where "Id" = $1';
-const getAllEpisodes = 'select * from "Episodes"';
+const getEpisodeBySerialId = 'select * from "Episodes" where "SerialId" = $1';
 
 
 module.exports = {
@@ -13,5 +13,5 @@ module.exports = {
   updateEpisode,
   findEpisode,
   findEpisodeById,
-  getAllEpisodes
+  getEpisodeBySerialId
 };
